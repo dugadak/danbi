a24f63560a643051040eef8bd3b4723907093612
dugadak <yya71bb@naver.com>
2023-06-15 13:24:49 +0900

    feat: User Signin

--- User/views.py
+++ User/views.py
@@ -28,3 +28,19 @@ class UserViewSet(viewsets.ModelViewSet):
                             status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    @action(detail=False, methods=['post'])
+    def signin(self, request):
+        user_id = request.data.get('user_id')
+        password = request.data.get('password')
+
+        user = get_object_or_404(User, user_id=user_id)
+
+        if not user.check_password(password):
+            return Response({'error': 'id 혹은 password가 일치하지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+        
+        return Response({'token': access_token, 'refresh_token': refresh_token})
