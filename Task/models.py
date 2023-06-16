e443f1f00ce0235bf2b65062341ab1969ec941ef
dugadak <yya71bb@naver.com>
2023-06-17 01:44:56 +0900

    Feat: Change Task DB modeling

--- Task/models.py
+++ Task/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from User.models import Team, User
 
-class SubTask(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='TeamsubTasks')
-    is_complete = models.BooleanField(default=False)
-    completed_date = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    
+
 class Task(models.Model):
     create_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='CreatedTasks')
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='TeamTasks')
+    teams = models.ManyToManyField(Team, related_name='TeamTasks')
     title = models.CharField(max_length=20)
     content = models.TextField()
     is_complete = models.BooleanField(default=False)
-    completed_date = models.DateTimeField(auto_now=True)
+    completed_date = models.DateTimeField(default=None, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    SubTask = models.ForeignKey(SubTask, on_delete=models.CASCADE, related_name='MainTasks')
     
 
+class SubTask(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='TeamSubTasks')
+    is_complete = models.BooleanField(default=False)
+    completed_date = models.DateTimeField(default=None, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='SubTasks')
     
