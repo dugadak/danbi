b815cf15cafca221bfcebd2cc7a743065c008355
dugadak <yya71bb@naver.com>
2023-06-17 01:37:31 +0900

    Fix: user_id -> username

--- User/views.py
+++ User/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import User
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import viewsets, status
 from User.serializer import UserSerializer
+from User.models import User
 from django.shortcuts import get_object_or_404
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -31,10 +31,10 @@ class UserViewSet(viewsets.ModelViewSet):
         
     @action(detail=False, methods=['post'])
     def signin(self, request):
-        user_id = request.data.get('user_id')
+        username = request.data.get('username')
         password = request.data.get('password')
 
-        user = get_object_or_404(User, user_id=user_id)
+        user = get_object_or_404(User, username=username)
 
         if not user.check_password(password):
             return Response({'error': 'id 혹은 password가 일치하지 않습니다.'}, status=status.HTTP_401_UNAUTHORIZED)
